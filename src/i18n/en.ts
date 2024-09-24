b8ed8f74aa40caae0ea0538116606c0a29c8e4c8
chuhemiao <535101602@qq.com>
2024-09-24 10:51:02 +0800

    feat: faq

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -9,29 +9,34 @@ export default {
 
   featureList: [
     {
-      title: 'Advanced Tech Stack',
+      title: 'Comprehensive Curriculum',
       description:
-        'We use the latest technology of each public chain to teach, students learn while doing hackathon and projects, you can quickly accumulate experience and web3 knowledge.'
+        'From technology, product to operation, covering a full range of practical courses for Web3 transformation, helping you quickly transform Web3.'
     },
     {
-      title: 'Interview 1V1 Coaching',
+      title: 'Diversified Learning Programs',
       description:
-        'After each 1v1 participant enters the real-world program, Gold Headhunters help you customize your fit.'
+        'We offer pure enjoyment classes, small group classes, VIP boutique classes and SVIP guaranteed classes to meet the needs of students from different backgrounds and find the most suitable way of learning.'
     },
     {
-      title: 'Supported by Rust, Ethereum, ICP.',
+      title: 'Super Tutor Team',
       description:
-        'IB offers several programs that can be adapted to the needs of each student.'
+        'The technical tutors are experienced, specializing in cross-chain development and smart contracts, and have led the team to win a number of grants. the headhunting tutors come from famous exchanges and have extensive resources, which help the students to transform precisely and take high-paying positions.'
     },
     {
-      title: 'Web3 Project for Enterprise Mainnet',
+      title: 'Career development support',
       description:
-        'After going through some series of learning, students participate in projects that are real enterprise-level projects deployed in the main network and incubated by professional organizations.'
+        'We provide interview escort services, including resume optimization, mock interviews, post-interview review, salary negotiation and onboarding guidance, to help you succeed in the workplace.'
     },
     {
-      title: 'SVIP Guaranteed Class',
+      title: 'Real project experience',
       description:
-        'Worried about not finding a job, IB offered an 80% refund if you could not find one.'
+        'Practical teaching, participate in real Web3 projects from 0 to 1, mentors lead students to do hackathon projects and participate in real enterprise-level projects deployed in the main network to quickly accumulate practical experience.'
+    },
+    {
+      title: 'Borderless Digital Nomadic Community',
+      description:
+        'Provides Web3 industry research, English learning dry goods and digital nomad information to help free work and life.'
     }
   ],
   featuresDesc: 'Easy to learn, fast to shore.',
@@ -40,6 +45,17 @@ export default {
       question: 'What is IBuidl?',
       answer:
         'The term Indie Buidl comes from Indie Hacker, and the use of buidl here is a slang term for the web3 industry, so indiebuidl can also be an independent developer in the web3 industry, they are a group of remote workers who have their own craft and can earn by participating in the hackathon, and at the same time, they may be a contributor to the open-source public chain or project, so indiebuidl is a group of creative, independent thinkers.'
+    },
+    {
+      question:
+        'What are the career opportunities as well as salaries in the Web3 industry?',
+      answer:
+        'There are many job opportunities and higher demand for some niche fields. Here is a rough look at the technical and non-technical aspects, the average annual salary of Web3 technicians is 140,000 ~ 200,000 USD (equivalent to RMB 1,000,000 ~ 1,450,000 USD), and the average annual salary of Web3 non-technical staff is 125,000 ~ 167,000 USD (equivalent to RMB 9,000,000 ~ 1,200,000 USD). The salary depends on the type of work, years of experience, location and qualifications. For more salary details, please check ibuidl SOP.'
+    },
+    {
+      question: 'Trends and transition directions for JAVA engineers?',
+      answer:
+        'Buy IBuidl  Rust 5V1 course and we will send the Solidity course directly to you for free! There is no price increase! For the price that you would have paid for the Rust technology course only, you now get an additional free Solidity technology class! Get access to both technologies and master Rust and Solidity at the same time!'
     }
   ]
 };
